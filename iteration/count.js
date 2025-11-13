c976c208d46fb1e691df89ebacb8bd7a0aa76da1
Emmanuel <emmanuellumbasi78@gmail.com>
2025-11-14 00:33:52 +0100

    Intial commit of the created files
    with some practice Exercises

---
+++ iteration/count.js
@@ -0,0 +1,17 @@
+/*
+# Sentinel
+Keep on asking for input (`float`) until a zero is entered.
+Count and print how many non-zero inputs you received.
+
+Counting always results in integers. Hence, there is no need to print any digits after the decimal point.
+
+## Example:
+
+    > 1.4
+    > -2.1
+    > 3.0
+    > -4
+    > 0
+    4
+
+*/
